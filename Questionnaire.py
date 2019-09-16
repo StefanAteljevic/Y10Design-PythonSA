1efd5c4d7a1fa06981da03e5c695609b93d0c510
Stefan Ateljevic <stefan.ateljevic@GCCYJPYJJ1WK.local>
2019-09-16 09:41:21 -0400

    Questionnaire

---
+++ Questionnaire.py
@@ -0,0 +1,28 @@
+# Here is a program to show how to use "if - elif - else"
+# The hash-tag is used to show that these are comments
+# This means that the computer will not look at these lines
+# Author: Mr. Jugoon
+# Upper Canada College
+
+# Put down some options for the user to choose from...
+
+print("1. Weather")
+print("2. Date")
+print("3. President of Yugoslavia from 1953 - 1980")
+print(" ")
+
+choice = int(input("What would you like to learn about? Please pick a number from 1 to 3.\n"))
+
+if choice == 1:
+    print("It is rainy and cloudy!")
+elif choice == 2:
+    print ("It is September 16, 2019!")
+elif choice == 3:
+    print ("TITO!")
+else:
+    print ("This is not a valid choice")
+    print ("Hope you have a great day anyway!")
+
+
+# This is a way to gracefuuly exit the program
+input("Press ENTER to quit the program")
