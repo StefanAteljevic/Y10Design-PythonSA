a2a34fdc29a7168a8d0aed50ea2912d0e1c0eb50
Stefan Ateljevic <stefan.ateljevic@GCCYJPYJJ1WK.local>
2019-10-15 20:26:23 -0400

    Updated Star Wars Api html

--- jsontest1.py
+++ jsontest1.py
@@ -21,30 +21,55 @@ import json
 # For any indentation errors, make sure there are no tabs (\t) by doing 
 # a full replace of \t with 4 actual spaces
 
-def writeHTML(data):
+#Which information would you like?
+#Movies, characters, planets.
+#User will select the one that they want. 
+
+def writeHTML(titles, people):
     myfile = open("myapi.html","w")
-    myfile.write("<h1>JSON file returned by API call</h1>")
-    myfile.write("<p>Copy and paste to <a href='https://jsoneditoronline.org/'>JSON editor</a> for pretty format.</p>")
-    myfile.write(data)
+    myfile.write("<h1> Star Wars Data, Facts, and Statistics</h1>")
+    for i in range(len(titles)):
+        myfile.write(f"<h1>{titles[i]}</h1>")
+    for i in range(len(people)):
+        myfile.write(f"<h1>{people[i]}</h1>")
+    # myfile.write("<h1>" + titles[0] + "</h1>")
+    # myfile.write("<h1>JSON file returned by API call</h1>")
+    # myfile.write("<p>Copy and paste to <a href='https://jsoneditoronline.org/'>JSON editor</a> for pretty format.</p>")
+    # myfile.write(data)
     myfile.close()
 
 def main():
     # use API to get place info
-    response = requests.get("https://swapi.co/api/films/")
+    movies = requests.get("https://swapi.co/api/films/")
+    people = requests.get("https://swapi.co/api/people/")
+
 
     # if API call is correct
-    if (response.status_code == 200):
-        data = response.content # response comes in as byte data type
+    if (movies.status_code == 200):
+        data = movies.content # response comes in as byte data type
         data_as_str = data.decode()    # decode to str
-        writeHTML(data_as_str)  # call function to write string data to HTML file
-        datajson = response.json()
+          # call function to write string data to HTML file
+        datajson = movies.json()
         # print(data)
 
         res = datajson["results"]
+        peopleres = people.json()["results"]
 
+        people = []
+        for person in peopleres:
+            name = person["name"]
+            people.append(name)
+            # print(person)
+
+
+        titles = []
         for movie in res:
             title = movie['title']
-            print(title)
+            titles.append(title)
+            # print(movie)
+
+
+        writeHTML(titles, people)
 
         # load as a JSON to access specific data more easily
         # datajson = response.json()
